7d0d01fc1a82621dac5a3cb176c70ae43c2286bd
Paragsorte <33374656+Paragsorte@users.noreply.github.com>
2017-12-03 20:49:56 -0800

    Add files via upload

---
+++ Assignment_2_Static_table_feed_file_1.sh
@@ -0,0 +1,27 @@
+#  Local Path   :   /home/cloudera/workspace/Parag/Assignment2
+# HDFS Path    :  hadoop fs -mkdir /user/hive//Assignment2/hiveAssignment1
+
+
+
+#!/bin/bash
+clear
+
+
+rm -r /home/cloudera/workspace/Parag/Assignment2/file_generated
+
+mkdir /home/cloudera/workspace/Parag/Assignment2/file_generated
+
+cd /home/cloudera/workspace/Parag/Assignment2/file_generated
+
+TodayDate=$1
+for ((i = 1; i <= 900; i++))
+do
+s=$TodayDate"|"string1"|"string2"|"string3
+for ((k=1; k<=496; k++ ))
+do
+   s=$s"|"$k 
+done
+echo $s >> $2
+done
+  
+    
